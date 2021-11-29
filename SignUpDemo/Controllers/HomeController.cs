d685ef59d5f1bb6a9fe1297f2ee1b83758a33092
abz322 <abz322@hotmail.co.uk>
2021-11-29 18:02:41 +0000

    Moved functions to utility class

--- SignUpDemo/Controllers/HomeController.cs
+++ SignUpDemo/Controllers/HomeController.cs
@@ -1,27 +1,19 @@
-﻿using SignUpDemo.Models;
+using SignUpDemo.Models;
 using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
-using System.Net.Mail;
-using System.Security.Cryptography;
-using System.Text;
-using System.Web;
 using System.Web.Mvc;
 
 namespace SignUpDemo.Controllers
 {
     public class HomeController : Controller
     {
-        private string connStr = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
-        // GET: Home
 
         public ActionResult Test()
         {
-            List<UserDetails> userList = GetUserDetails();
+            Utils ut = new Utils();
+            List<UserDetails> userList = ut.GetUserDetails();
             ViewBag.UserList = userList;
             return PartialView();
         }
@@ -36,13 +28,14 @@ namespace SignUpDemo.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Index(UserDetails model)
         {
+            Utils ut = new Utils();
             Result result = new Result();
             try
             {
                 if (ModelState.IsValid)
                 {
                     //Regex is only ideal for simple email validation, so perform a in depth validation
-                    if (!ValidateEmailAddress(model.Email))
+                    if (!ut.ValidateEmailAddress(model.Email))
                     {
                         result.Status = (int)HttpStatusCode.BadRequest;
                         result.Message = "This email is not valid.";
@@ -51,7 +44,7 @@ namespace SignUpDemo.Controllers
 
                     //The password needs to be stored securely, using a hash value over encryption is the best way to do this
                     //to avoid exposing an encryption key for the passwords
-                    byte[] hashedPassword = HashPassword(model.Password);
+                    byte[] hashedPassword = ut.HashPassword(model.Password);
 
                     if (hashedPassword == null)
                     {
@@ -62,7 +55,7 @@ namespace SignUpDemo.Controllers
 
                     var userDet = new UserDetails();
 
-                    List<UserDetails> userDetails = GetUserDetails(model.Email);
+                    List<UserDetails> userDetails = ut.GetUserDetails(model.Email);
 
                     bool userExists = userDetails.Any();
 
@@ -73,7 +66,7 @@ namespace SignUpDemo.Controllers
                         throw new Exception();
                     }
 
-                    int addUserResult = AddUserDetails(model.Email, hashedPassword);
+                    int addUserResult = ut.AddUserDetails(model.Email, hashedPassword);
 
                     if (addUserResult == 0)
                     {
@@ -102,124 +95,6 @@ namespace SignUpDemo.Controllers
             }
         }
 
-        private int AddUserDetails(string email, byte[] password)
-        {
-            int result = 0;
-            try
-            {
-                using (SqlConnection con = new SqlConnection(connStr))
-                {
-                    con.Open();
-
-                    string sql = "INSERT INTO [UserDetails] (Email,Password) VALUES(@email,@password)";
-                    using (SqlCommand cmd = new SqlCommand(sql, con))
-                    {
-                        cmd.Parameters.Add("@email", SqlDbType.VarChar, 50).Value = email;
-                        cmd.Parameters.Add("@password", SqlDbType.Binary, 32).Value = password;
-                        cmd.CommandType = CommandType.Text;
-                        result = cmd.ExecuteNonQuery();
-                    }
-                }
-                return result;
-            }
-            catch
-            {
-                return result;
-            }
-        }
-
-        private List<UserDetails> GetUserDetails(string email = null)
-        {
-            List<UserDetails> userList = new List<UserDetails>();
-            try
-            {
-                using (SqlConnection con = new SqlConnection(connStr))
-                {
-                    SqlCommand cmd;
-                    if(email == null)
-                    {
-                        cmd = new SqlCommand("SELECT * FROM [UserDetails]", con);
-                    }
-                    else
-                    {
-                        cmd = new SqlCommand("SELECT * FROM [UserDetails] WHERE Email = @email", con);
-                        cmd.Parameters.Add("@email", SqlDbType.VarChar, 50).Value = email;
-                    }
-                    cmd.CommandType = CommandType.Text;
-                    con.Open();
-
-                    SqlDataReader rdr = cmd.ExecuteReader();
-                    while (rdr.Read())
-                    {
-                        var userDet = new UserDetails();
-
-                        userDet.UserID = Convert.ToInt32(rdr["UserID"]);
-                        userDet.Email = rdr["Email"].ToString();
-                        byte[] hashValue = (byte[])rdr["Password"];
-                        StringBuilder hashResult = new StringBuilder();
-                        for (int i = 0; i < hashValue.Length; i++)
-                        {
-                            hashResult.Append(hashValue[i].ToString("x2"));
-                        }
-                        userDet.Password = hashResult.ToString();
-                        userList.Add(userDet);
-                    }
-                }
-                return userList;
-            }
-            catch
-            {
-                return userList;
-            }
-        }
-
-        private byte[] HashPassword(string password)
-        {
-            using (SHA256 shaHasher = SHA256.Create())
-            {
-                try
-                {
-                    byte[] hashValue = shaHasher.ComputeHash(Encoding.UTF8.GetBytes(password));
-                    return hashValue;
-                }
-                catch
-                {
-                    return null;
-                }
-            }
-        }
-
-        private bool ValidateEmailAddress(string emailAddress)
-        {
-            try
-            {
-                MailAddress address = new MailAddress(emailAddress);
-                if(address.Address == emailAddress)
-                {
-                    return true;
-                }
-                return false;
-            }
-            catch (FormatException)
-            {
-                return false;
-            }
-        }
-
-        private bool VerifyPassword(byte[] passToVerify, byte[] storedPass)
-        {
-            try
-            {
-                if (passToVerify.SequenceEqual(storedPass))
-                {
-                    return true;
-                }
-                return false;
-            }
-            catch
-            {
-                return false;
-            }
-        }
+        
     }
 }
