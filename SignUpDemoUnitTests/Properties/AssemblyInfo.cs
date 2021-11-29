09db90e7f0a57358d52fa5986fadc7a54a4a3a49
abz322 <abz322@hotmail.co.uk>
2021-11-29 18:52:54 +0000

    Fixed a deployment issue caused by missing files from a faulty github upload

---
+++ SignUpDemoUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SignUpDemoUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SignUpDemoUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("df4b69c7-4bec-4bf4-a520-68b5d0c2b499")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SignUpDemoUnitTests/SignUpUT.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using SignUpDemo.Controllers;
+using System.Text;
+
+namespace SignUpDemo.Tests
+{
+    [TestClass]
+    public class SignUpUT
+    {
+        [TestMethod]
+        public void TestVerifyPassword()
+        {
+            Utils ut = new Utils();
+
+            //Byte array for the string "test"
+            byte[] byteArrTest = new byte[] { 116, 101, 115, 116 };
+            string stringTest = "test";
+
+            bool result = ut.VerifyPassword(Encoding.UTF8.GetBytes(stringTest), byteArrTest);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestHashPassword()
+        {
+            Utils ut = new Utils();
+            string password = "Test1234";
+            string hashedPassword = "07480fb9e85b9396af06f006cf1c95024af2531c65fb505cfbd0add1e2f31573";
+
+            byte[] byteArr = ut.HashPassword(password);
+            StringBuilder hashResult = new StringBuilder();
+            for (int i = 0; i < byteArr.Length; i++)
+            {
+                hashResult.Append(byteArr[i].ToString("x2"));
+            }
+
+            string result = hashResult.ToString();
+
+            Assert.AreEqual(hashedPassword, result);
+        }
+
+        [TestMethod]
+        public void TestValidateEmail()
+        {
+            Utils ut = new Utils();
+            string email = "test@test.co.uk";
+            bool result = ut.ValidateEmailAddress(email);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
